55d57398d24a90a7a908a2c2d2a5cd9e75c2bdaa
User <alinatyndyk777@gmail.con>
2022-10-19 15:13:54 +0300

    Fetch 2 lesson + hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {useForm} from "react-hook-form";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const {register, handleSubmit} = useForm();
+    const submit = (obj) => {
+        console.log(obj);
+
+        fetch('https://jsonplaceholder.typicode.com/posts',{
+            method: 'POST',
+            body: JSON.stringify(obj),
+            headers: {'Content-type': 'application/json; charset=UTF-8'}
+        })
+            .then(res => res.json())
+            .then(res => console.log(res))
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(submit)}>
+                <input type="text" {...register('title', {required: true})}/>
+                <input type="text" {...register('body')}/>
+                <select {...register('userId')}>
+                    <option value='1'>one</option>
+                    <option value='2'>two</option>
+                    <option value='3'>three</option>
+                </select>
+                <button>save</button>
+            </form>
+
+        </div>
+    );
 }
 
 export default App;
